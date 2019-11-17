8d4c924d3bd4f7f93682b2d2e60f763b1feda44e
surkat919 <surkat919@clouddefense.ai>
2019-11-16 21:10:01 -0800

    initial

---
+++ src/main/java/com/clouddefense/examples/VulnerableApp.java
@@ -0,0 +1,11 @@
+package com.clouddefense.examples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VulnerableApp {
+    public static void main(String[] args) {
+        SpringApplication.run(VulnerableApp.class, args);
+    }
+}
